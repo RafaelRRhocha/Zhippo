af32564aec8fc374130287dd43ba5a302e8a4929
Rafael Rocha <rafael.rocha@umode.com.br>
2025-05-16 15:00:36 -0300

    Update project version to 1.1.4, add TrackingPage component, and enhance Header component with new navigation links and mounted state management.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,21 @@
 
 import {
   ActionIcon,
+  Anchor,
   useComputedColorScheme,
   useMantineColorScheme,
 } from '@mantine/core';
 import { IconMoon, IconSun } from '@tabler/icons-react';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 const Header = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
   const { setColorScheme } = useMantineColorScheme({
     keepTransitions: true,
   });
@@ -20,26 +28,50 @@ const Header = () => {
     setColorScheme(computedColorScheme === 'light' ? 'dark' : 'light');
   };
 
-  return (
-    <header className="flex w-full items-center justify-between px-4 shadow-sm">
-      <div className="flex items-center gap-x-2 py-2">
-        <Image src="/logo.png" alt="Zhippo" width={70} height={70} />
-        <h1 className="text-xl font-bold">Zhippo</h1>
-      </div>
+  if (!isMounted) return null;
 
-      <ActionIcon
-        onClick={toggleColorScheme}
-        variant="default"
-        size="xl"
-        aria-label="Toggle color scheme"
-      >
-        {computedColorScheme === 'light' ? (
-          <IconMoon className="size-6" />
-        ) : (
-          <IconSun className="size-6" />
-        )}
-      </ActionIcon>
-    </header>
+  return (
+    isMounted && (
+      <header className="flex w-full items-center justify-between px-4 shadow-sm">
+        <div className="flex items-center gap-x-2 py-2">
+          <Image src="/logo.png" alt="Zhippo" width={70} height={70} />
+          <h1 className="text-xl font-bold">Zhippo</h1>
+        </div>
+
+        <div className="flex items-center gap-x-5">
+          <Anchor
+            href="/"
+            variant="gradient"
+            gradient={{ from: 'blue', to: 'cyan' }}
+            className="text-sm font-medium"
+          >
+            Calculadora de Frete
+          </Anchor>
+
+          <Anchor
+            href="/tracking"
+            variant="gradient"
+            gradient={{ from: 'blue', to: 'cyan' }}
+            className="text-sm font-medium"
+          >
+            Rastreamento
+          </Anchor>
+
+          <ActionIcon
+            onClick={toggleColorScheme}
+            variant="default"
+            size="xl"
+            aria-label="Toggle color scheme"
+          >
+            {computedColorScheme === 'light' ? (
+              <IconMoon className="size-6" />
+            ) : (
+              <IconSun className="size-6" />
+            )}
+          </ActionIcon>
+        </div>
+      </header>
+    )
   );
 };
 
